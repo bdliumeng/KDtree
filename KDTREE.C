996137bbda4a706ed0c2cbf31a26e2c9ff81df39
rohitjain1125 <rohit.jain1125@gmail.com>
2013-04-16 15:41:42 +0530

    Update KDTREE.C
    Used as a Library File

--- KDTREE.C
+++ KDTREE.C
@@ -4,14 +4,14 @@
 #include<time.h>
 struct node  
 {
-  int data1;
+	int data1;
 	int data2;
 	struct node *left;
 	struct node *right;
 };
+
 void insert(struct node** node,int key1,int key2,int level)
 {
-	//static int level=0;
 	if(level%2==0)
 	{	
 		if((*node)==NULL)
@@ -21,7 +21,6 @@ void insert(struct node** node,int key1,int key2,int level)
 			(*node)->data2=key2;
 			(*node)->left=NULL;
 			(*node)->right=NULL;
-			//(*node)->level=0;
 			//printf("Inserted Node with data %d   %d\n",(*node)->data1,(*node)->data2);
 		}
 		else if( (*node)->data1 > key1)
@@ -44,7 +43,6 @@ void insert(struct node** node,int key1,int key2,int level)
 			(*node)->data2=key2;
 			(*node)->left=NULL;
 			(*node)->right=NULL;
-			//(*node)->level=0;
 			//printf("Inserted Node with data %d   %d\n",(*node)->data1,(*node)->data2);
 		}
 		else if( (*node)->data2 > key2)
@@ -71,7 +69,6 @@ void search_nearest_neighbour(struct node** node,int key1,int key2,int level)
 			(*node)->data2=key2;
 			(*node)->left=NULL;
 			(*node)->right=NULL;
-			//(*node)->level=0;
 			printf("For Node of data %d   %d\n",(*node)->data1,(*node)->data2);
 		}
 		else if( (*node)->data1 > key1)
@@ -98,7 +95,6 @@ void search_nearest_neighbour(struct node** node,int key1,int key2,int level)
 			(*node)->data2=key2;
 			(*node)->left=NULL;
 			(*node)->right=NULL;
-			//(*node)->level=0;
 			printf("For Node of data %d   %d\n",(*node)->data1,(*node)->data2);
 		}
 		else if( (*node)->data2 > key2)
@@ -125,45 +121,3 @@ void inorder(struct node* node)
 		inorder(node->right);
 	}
 }
-int main()
-{
-	struct node* root =NULL;
-	/*insert(&root,20,30,0);
-	insert(&root,25,40,0);
-	insert(&root,30,25,0);
-	insert(&root,26,45,0);
-	insert(&root,15,60,0);
-	insert(&root,18,40,0);
-	insert(&root,19,70,0);
-	insert(&root,50,30,0);
-	inorder(root);
-	search_nearest_neighbour(&root,50,50,0);
-	return 0;*/
-	clock_t start, end;
-	double duration = 0,duration2=0;
-	start=clock();
-	long a=1000000,r,s;
-	srand(time(NULL));
-	while(a--)
-	{	
-		long r,s;
-		r=rand();
-		s=rand();
-		insert(&root,r,s,0);
-	}
-	end=clock();
-	duration = (((float)end - (float)start) / CLOCKS_PER_SEC ) ;  
-	printf("TIme to Create is %f  Sec\n",duration);	
-//	inorder(root);
-	r=rand();
-	s=rand();
-	clock_t t1,t2;
-	t1=clock();
-	search_nearest_neighbour(&root,r,s,0);
-	t2=clock();
-	duration2 = ((((float)t2 - (float)t1) / CLOCKS_PER_SEC )*1000);  
-	printf("TIme to Search is %f  sec\n",duration2);		
-	return 0;
-}
-
-	
