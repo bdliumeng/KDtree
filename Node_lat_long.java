7d78edd5614dbe1e0b9ec06a92ad73746da3648b
rohitjain1125 <rohit.jain1125@gmail.com>
2013-04-22 13:51:56 +0530

    Create Node_lat_long.java

---
+++ Node_lat_long.java
@@ -0,0 +1,51 @@
+
+public class Node
+{
+  private	Node left;
+	private Node right;
+	private double latitude;
+	private double longitude;
+	private double zipcode;
+	private String city;
+	private	String state;
+
+	public Node(double latitude,double longitude,int level,double zipcode,String city,String state)
+	{
+		this.latitude=latitude;
+		this.longitude=longitude;
+		this.zipcode=zipcode;
+		this.city=city;
+		this.state=state;
+		left=null;
+		right=null;
+	}
+	public Node(double latitude,double longitude,int level)
+	{
+		this.latitude=latitude;
+		this.longitude=longitude;
+		left=null;
+		right=null;
+		
+	}
+	public void setRight(Node right)
+	{
+		this.right=right;
+	}	
+	public void setLeft(Node left)
+	{
+		this.left=left;
+	}	
+	public Node getLeft(){return left;}
+	public Node getRight(){return right;}
+	//public void setKey(int k1,int k2){this.key1=k1,this.key2=k2;}
+	public double getlatitude(){return latitude;}
+	public double getlongitude(){return longitude;}
+	public double getzipcode(){return zipcode;}
+	public String getcity(){return city;}
+	public String getstate(){return state;}
+	public void print()
+	{
+		
+		System.out.println("Keys are:"+getlatitude()+","+getlongitude());
+	}
+}
